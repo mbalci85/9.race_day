2d22a2eddfbb8d627e28a7f101ed3693538da182
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-20 23:49:22 +0000

    prompt and alert added

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 //racenumber is more than 1000 for early registered adults, the rest receives less than 1000
 let raceNumber;
 
-// this shows if the runner registers or not
-let earlyBird = false;
-
 //there are adult and young runners
-let runnerAge = 18;
+// let runnerAge = 18;
+let runnerAge = prompt('Enter your age:');
 
-if (earlyBird && runnerAge > 18) {
+// this shows if the runner registers or not
+// let earlyBird = false;
+let earlyBird = prompt(
+	'Enter registration status: for early reg enter true, for late reg enter false',
+);
+
+if (earlyBird == 'true' && runnerAge > 18) {
 	raceNumber = Math.ceil(Math.random() * 1000 + 1000);
-	console.log(
-		`You will run at 09:30 am and your race number is ${raceNumber}`,
-	);
-} else if (!earlyBird && runnerAge > 18) {
+	alert(`You will run at 09:30 am and your race number is ${raceNumber}`);
+} else if (earlyBird == 'false' && runnerAge > 18) {
 	raceNumber = Math.ceil(Math.random() * 1000);
-	console.log(
-		`You will run at 11:00 am and your race number is ${raceNumber}`,
-	);
+	alert(`You will run at 11:00 am and your race number is ${raceNumber}`);
 } else if (runnerAge < 18) {
 	raceNumber = Math.ceil(Math.random() * 1000);
-	console.log(
-		`You will run at 12:30 am and your race number is ${raceNumber}`,
-	);
+	alert(`You will run at 12:30 am and your race number is ${raceNumber}`);
 } else if (runnerAge == 18) {
-	console.log(`See the registration desk....`);
+	alert(`See the registration desk....`);
 }
